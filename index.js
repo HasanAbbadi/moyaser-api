5fc288e947c5786a6d685a1bffc9b31e2ee8dd74
HasanAbbadi <hasanabady55@gmail.com>
2023-03-05 16:28:27 +0200

    changed output from nested objects to nested arrays

--- index.js
+++ index.js
@@ -44,20 +44,23 @@ app.get("/page", (req, res) => {
 });
 
 app.get("/page/:query", (request, response) => {
-  const query = request.params.query;
-  const groupByPage = groupBy("page_number");
-  const pages = groupByPage(data);
-  const result = pages[query];
-  const groupByChapter = groupBy("chapter_id");
-  const chapters = groupByChapter(result);
-  Object.keys(chapters).forEach(function (key) {
-    const groupByVerse = groupBy('verse_key');
-    chapters[key] = groupByVerse(chapters[key]);
+  const query = request.params.query - 1;
+
+  if (query > 603) response.sendStatus(404);
+
+  // classifying all objects and grouping them by values
+  let pages = groupBy(data, (a, b) => a.page_number === b.page_number);
+  pages = pages[query];
+  const chapters = groupBy(pages, (a, b) => a.chapter_id === b.chapter_id);
+
+  // group words into verse array in each chapter array
+  chapters.map((_, i) => {
+    chapters[i] = groupBy(chapters[i], (a, b) => a.verse_key === b.verse_key);
   });
-  response.send({
-    page: query,
-    chapters: chapters,
-  });
+
+  // send the final array of 3 nested arrays
+  // chapters:[] -> verses:[] -> words:[] -> word:{}
+  response.send(chapters);
 });
 
 app.get("/verse", (_, res) => {
@@ -65,24 +68,47 @@ app.get("/verse", (_, res) => {
 });
 
 app.get("/verse/:query", (request, response) => {
-  const query = request.params.query;
-  const groupByVerse = groupBy("verse_id");
-  const verses = groupByVerse(data);
+  const query = request.params.query - 1;
+
+  if (query > 6235) response.sendStatus(404);
+
+  // group all objects by verse number.
+  const verses = groupBy(data, (a, b) => a.verse_id === b.verse_id);
+
+  // send the final object that holds the words array:
+  // info:{} -> words:[] -> word:{}
   response.send({
     page: verses[query][0].page_number,
     chapter: verses[query][0].chapter_id,
-    verses: verses[query],
+    words: verses[query],
   });
 });
 
-const groupBy = (key) => (array) =>
-  array.reduce(
-    (objectsByKeyValue, obj) => ({
-      ...objectsByKeyValue,
-      [obj[key]]: (objectsByKeyValue[obj[key]] || []).concat(obj),
-    }),
-    {}
-  );
+function groupBy(arr, cb) {
+  let list = [...arr];
+  const result = [];
+  while (list.length) {
+    const { alike, remaining } = group(list, cb);
+    result.push(alike);
+    list = remaining;
+  }
+  return result;
+}
+
+function group(list, cb) {
+  const alike = [];
+  const remaining = [];
+  const a = list.shift();
+  list.map((b) => {
+    const bool = cb(a, b);
+    if (bool) alike.push(b);
+    else remaining.push(b);
+  });
+  return {
+    alike: alike.length > 0 ? [a, ...alike] : a,
+    remaining,
+  };
+}
 
 app.listen(port, () => console.log(`Server started at ${port}`));
 
